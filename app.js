11d4cf112d0d5dbdb8dff3075e8f9e4093a19e36
liang lv <517486047@qq.com>
2018-05-07 22:02:14 +0800

    '新增模板'

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const Koa = require('koa')
+const path = require('path')
 const bodyparser = require('koa-bodyparser')
+const nunjuncks = require('koa-nunjucks-2')
+const staticFiles = require('koa-static')
+
 const app = new Koa()
 const router = require('./router')
 
-app.use(bodyparser())
+app.use(staticFiles(path.resolve(__dirname, './public')))
 
+app.use(nunjuncks({
+    ext: 'html',
+    path: path.join(__dirname, 'views'),
+    nunjucksConfig: {
+        trimBlocks: true
+    }
+}))
+
+app.use(bodyparser())
 router(app)
 
 app.listen(3000, () => {
--- controller/home.js
+++ controller/home.js
@@ -3,7 +3,9 @@ const HomeServer = require('../service/home')
 
 module.exports = {
   index: async (ctx, next) => {
-    ctx.response.body = `<h1>index</h1>`
+    await ctx.render('home/index', {
+      title: '欢迎你回来'
+    })
   },
   home: async (ctx, next) => {
     console.log(ctx.request.qurey)
@@ -15,25 +17,20 @@ module.exports = {
     ctx.response.body = `<h1>home/:id/:name</h1>`
   },
   login: async (ctx, next) => {
-    ctx.response.body =
-      `
-          <form action="/user/register" method="post">
-            <input name="name" type="text" placeholder="请输入用户名：ikcamp"/> 
-            <br/>
-            <input name="password" type="text" placeholder="请输入密码：123456"/>
-            <br/> 
-            <button>GoGoGo</button>
-          </form>
-        `
+    await ctx.render('home/login', {
+      btnName: 'Go'
+    })
   },
   register: async (ctx, next) => {
-    let {
-      name,
-      password
-    } = ctx.request.body
-    let data = await HomeServer.register(name, password)
+    let { name, password } = ctx.request.body
+    let res = await HomeServer.register(name, password)
 
-    ctx.response.body = data
+    if(res.status == "-1"){
+      await ctx.render("home/login", res.data)
+    }else{
+      ctx.state.title = "个人中心"
+      await ctx.render("home/success", res.data)
+    }
   },
   404: async (ctx, next) => {
     ctx.response.body = `<h1>404</h1>`
